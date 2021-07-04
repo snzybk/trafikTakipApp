5e1bba2520b863422040752295a5a2d7f322a6e9
snzybk <75361750+snzybk@users.noreply.github.com>
2021-07-04 17:05:20 +0300

    first committ

--- app/src/main/java/com/example/mappapp/MainActivity.java
+++ app/src/main/java/com/example/mappapp/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
     FusedLocationProviderClient fusedLocationProviderClient;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
